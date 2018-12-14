eeac303a57496abbd308f48221635148181bd3bf
SametDemir686 <43796464+SametDemir686@users.noreply.github.com>
2018-12-14 15:02:28 +0300

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,1 +1,23 @@
 
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Enemy extends Soldier {
+
+	public Enemy(boolean movable, int x, int y) {
+		super(movable, x, y, Color.RED);
+	}
+
+	@Override
+	public void draw(Graphics g, int initialXShift, int initialYShift, int squareHeight, int squareWidth) {
+		g.setColor(getColor());
+		g.fillOval(initialXShift + squareWidth * getX() + squareWidth / 3,
+				initialYShift + squareHeight * getY() + squareHeight / 3, squareWidth / 3, squareHeight / 3);
+	}
+
+	@Override
+	public int getWholeMapIndex() {
+		// TODO Auto-generated method stub
+		return Model.ENEMY;
+	}
+}
