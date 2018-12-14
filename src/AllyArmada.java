2914814c732c9ae22066c2af20da3d787fc709fb
SametDemir686 <43796464+SametDemir686@users.noreply.github.com>
2018-12-14 13:50:50 +0300

    Create AllyArmada.java

---
+++ src/AllyArmada.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class AllyArmada extends Ally {
+
+	public AllyArmada(boolean movable,int x, int y) {
+		super(movable, x, y);
+	}
+
+	@Override
+	public void draw(Graphics g, int initialXShift, int initialYShift, int squareHeight, int squareWidth) {
+		g.setColor(new Color(0,0,175,200));
+		g.fillRect(initialXShift + squareWidth * x, initialYShift + squareHeight * y, squareWidth, squareHeight);
+		g.setColor(getColor());
+		g.fillOval(initialXShift + squareWidth * getX() + squareWidth / 3,
+				initialYShift + squareHeight * getY() + squareHeight / 3, squareWidth / 3, squareHeight / 3);
+	}
+
+	@Override
+	public int getWholeMapIndex() {
+		// TODO Auto-generated method stub
+		return Model.ALLY_ARMADA;
+	}
+}
