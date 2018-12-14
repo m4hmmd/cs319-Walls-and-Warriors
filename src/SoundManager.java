2d81b405dab04eb45fa38fc77c9001da570c3b8f
SametDemir686 <43796464+SametDemir686@users.noreply.github.com>
2018-12-14 13:49:30 +0300

    Update SoundManager.java

--- src/SoundManager.java
+++ src/SoundManager.java
@@ -8,88 +8,99 @@ import java.io.*;
 
 public class SoundManager {
 
-    private static AudioPlayer BGMP;
-    private static AudioStream BGM;
-    private static AudioStream wallPlaced;
-    private static AudioStream gameWon;
-    private static AudioData BGMD;
-    private static ContinuousAudioDataStream loop;
+	private static AudioPlayer BGMP;
+	private static AudioStream BGM;
+	private static AudioStream wallPlaced;
+	private static AudioStream gameWon;
+	private static AudioStream mouseOver;
+	private static AudioData BGMD;
+	private static ContinuousAudioDataStream loop;
 
-    SoundManager() {
-        try {
-            BGMP = AudioPlayer.player;
-            BGM = new AudioStream(new FileInputStream("src/sounds/BGM.wav"));
-            BGMD = BGM.getData();
-            loop = new ContinuousAudioDataStream(BGMD);
-        } catch (IOException error) {
-            System.out.println(error.getMessage());
-        }
-    }
+	SoundManager() {
+		try {
+			BGMP = AudioPlayer.player;
+			BGM = new AudioStream(new FileInputStream("src/sounds/BGM.wav"));
+			BGMD = BGM.getData();
+			loop = new ContinuousAudioDataStream(BGMD);
+		} catch (IOException error) {
+			System.out.println(error.getMessage());
+		}
+	}
 
-    public static class wListener implements WindowListener {
+	public static class wListener implements WindowListener {
 
-        @Override
-        public void windowOpened(WindowEvent e) {
-            playBackgroundMusic();
-        }
+		@Override
+		public void windowOpened(WindowEvent e) {
+			playBackgroundMusic();
+		}
 
-        public void windowActivated(WindowEvent arg0) {
-        }
+		public void windowActivated(WindowEvent arg0) {
+		}
 
-        public void windowClosed(WindowEvent arg0) {
-        }
+		public void windowClosed(WindowEvent arg0) {
+		}
 
-        public void windowClosing(WindowEvent arg0) {
-        }
+		public void windowClosing(WindowEvent arg0) {
+		}
 
-        public void windowDeactivated(WindowEvent arg0) {
-        }
+		public void windowDeactivated(WindowEvent arg0) {
+		}
 
-        public void windowDeiconified(WindowEvent arg0) {
-        }
+		public void windowDeiconified(WindowEvent arg0) {
+		}
 
-        public void windowIconified(WindowEvent arg0) {
-        }
-    }
+		public void windowIconified(WindowEvent arg0) {
+		}
+	}
 
-    public static class bListener implements ActionListener {
+	public static class bListener implements ActionListener {
 
-        private boolean playing = false;
+		private boolean playing = false;
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            playing = !(playing);
-            if (playing) {
-                stopBackgroundMusic();
-            } else {
-                playBackgroundMusic();
-            }
-        }
-    }
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			playing = !(playing);
+			if (playing) {
+				stopBackgroundMusic();
+			} else {
+				playBackgroundMusic();
+			}
+		}
+	}
 
-    public static void gameWon() {
-        try {
-            gameWon = new AudioStream(new FileInputStream("src/sounds/gameWon.wav"));
-        } catch (IOException error) {
-            System.out.println(error.getMessage());
-        }
-        BGMP.start(gameWon);
-    }
+	public static void gameWon() {
+		try {
+			gameWon = new AudioStream(new FileInputStream("src/sounds/gameWon.wav"));
+		} catch (IOException error) {
+			System.out.println(error.getMessage());
+		}
+		BGMP.start(gameWon);
+	}
 
-    public static void wallPlaced() {
-        try {
-            wallPlaced = new AudioStream(new FileInputStream("src/sounds/wallPlaced.wav"));
-        } catch (IOException error) {
-            System.out.println(error.getMessage());
-        }
-        BGMP.start(wallPlaced);
-    }
+	public static void wallPlaced() {
+		try {
+			wallPlaced = new AudioStream(new FileInputStream("src/sounds/wallPlaced.wav"));
+		} catch (IOException error) {
+			System.out.println(error.getMessage());
+		}
+		BGMP.start(wallPlaced);
+	}
 
-    public static void playBackgroundMusic() {
-        BGMP.start(loop);
-    }
+	public static void playBackgroundMusic() {
+		// BGMP.start(loop);
+	}
 
-    public static void stopBackgroundMusic() {
-        BGMP.stop(loop);
-    }
+	public static void stopBackgroundMusic() {
+		BGMP.stop(loop);
+	}
+
+	public static void mouseOver() {
+		try {
+			mouseOver = new AudioStream(new FileInputStream("src/sounds/mouseOver.wav"));
+		} catch (IOException error) {
+			System.out.println(error.getMessage());
+		}
+		BGMP.start(mouseOver);
+
+	}
 }
