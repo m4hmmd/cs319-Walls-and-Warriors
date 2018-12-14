be5c89352cc3555c3ac8b914fc85b987cf5d1d34
SametDemir686 <43796464+SametDemir686@users.noreply.github.com>
2018-12-14 17:10:54 +0300

    Update Soldier.java

--- src/Soldier.java
+++ src/Soldier.java
@@ -3,10 +3,12 @@ import java.awt.*;
 public abstract class Soldier extends GameObject {
 
 	boolean movable;
+	Point initialPoint;
 
 	Soldier(boolean movable, int x, int y, Color c) {
 		super(x, y, c);
 		this.movable = movable;
+		initialPoint = new Point(x, y);
 	}
 
 	@Override
@@ -15,4 +17,14 @@ public abstract class Soldier extends GameObject {
 	@Override
 	public abstract int getWholeMapIndex();
 
+	public void sendToInitialPosition() {
+		x = initialPoint.x;
+		y = initialPoint.y;
+	}
+
+	public abstract boolean isArmada();
+
+	public boolean isInInitialPosition() {
+		return initialPoint.x == x && initialPoint.y == y;
+	}
 }
