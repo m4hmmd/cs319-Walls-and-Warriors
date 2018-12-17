c03a697dd1569f48deef27e27ac53824a0798c7b
Ahmet MALAL <32555715+AhmetMALAL@users.noreply.github.com>
2018-12-18 02:21:16 +0300

    Update Chain.java

--- src/Chain.java
+++ src/Chain.java
@@ -37,7 +37,7 @@ public class Chain extends WallOrChain {
 			g.fillRect(CoorX - healthWidth / 2, CoorY - healthHeight / 2 - lineWidth,
 					(int) (healthWidth * (health * 1.0 / initialHealth)), healthHeight);
 			Graphics2D g3 = (Graphics2D) g;
-			g3.setColor(Color.RED.brighter().brighter().brighter().brighter());
+			g3.setColor(Color.WHITE);
 			g3.draw(areaForSquare);
 
 		} else {
