a7e6a2689b488bffefa9444329bfccf38009bab3
Mahammad Shirinov <shrnvm@gmail.com>
2018-12-19 18:40:01 +0300

    add castle img

--- src/Castle.java
+++ src/Castle.java
@@ -1,11 +1,25 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.File;
 
 public class Castle extends GameObject {
+	private Image img;
 
 	int x1, y1;
 
 	Castle(int x, int y, int x1, int y1, Color c) {
 		super(x, y);
+
+		try
+		{
+			File file = new File("src/img/castle.png");
+			img = ImageIO.read(file);
+		}
+		catch ( Exception e )
+		{
+			System.out.println("Couldn't find file: " + e);
+		}
+
 		this.x1 = x1;
 		this.y1 = y1;
 	}
@@ -21,19 +35,22 @@ public class Castle extends GameObject {
 	void draw(Graphics g, int initialXShift, int initialYShift, int squareHeight, int squareWidth) {
 		int width = squareWidth / 5;
 		int height = squareHeight / 5;
-//		g.setColor(new Color(0, 255, 0));
 		g.setColor(Color.GRAY);
 		int leftOne = (getX() == getX1()) ? getX() : ((getX() > getX1()) ? getX1() : getX());
 		int topOne = (getY() == getY1()) ? getY() : ((getY() > getY1()) ? getY1() : getY());
 
-		g.fillOval(initialXShift + squareWidth * getX() + squareWidth / 4,
-				initialYShift + squareHeight * getY() + squareHeight / 4, squareWidth / 2, squareHeight / 2);
-		g.fillOval(initialXShift + squareWidth * getX1() + squareWidth / 3,
-				initialYShift + squareHeight * getY1() + squareHeight / 3, squareWidth / 3, squareHeight / 3);
+		if (img != null)
+			g.drawImage(img, initialXShift + squareWidth * x,
+					initialYShift + squareHeight * y, 2 * squareWidth, squareHeight, null);
 
-		g.fillRect(initialXShift + squareWidth * leftOne + squareWidth / 2 - ((getY() == getY1()) ? 0 : width / 2),
-				initialYShift + squareHeight * topOne + squareHeight / 2 - ((getX() == getX1()) ? 0 : height / 2),
-				(getX() == getX1()) ? width : (squareWidth), (getY() == getY1()) ? height : squareHeight);
+//		g.fillOval(initialXShift + squareWidth * getX() + squareWidth / 4,
+//				initialYShift + squareHeight * getY() + squareHeight / 4, squareWidth / 2, squareHeight / 2);
+//		g.fillOval(initialXShift + squareWidth * getX1() + squareWidth / 3,
+//				initialYShift + squareHeight * getY1() + squareHeight / 3, squareWidth / 3, squareHeight / 3);
+//
+//		g.fillRect(initialXShift + squareWidth * leftOne + squareWidth / 2 - ((getY() == getY1()) ? 0 : width / 2),
+//				initialYShift + squareHeight * topOne + squareHeight / 2 - ((getX() == getX1()) ? 0 : height / 2),
+//				(getX() == getX1()) ? width : (squareWidth), (getY() == getY1()) ? height : squareHeight);
 	}
 
 	@Override
